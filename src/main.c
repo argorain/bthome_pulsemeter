f8bc5e343112953585be6dbf2f000e5a9ac14dc5
Vojtech Vladyka <vojtech.vladyka@gmail.com>
2023-11-11 18:38:12 +0100

    This works

--- src/main.c
+++ src/main.c
@@ -6,10 +6,16 @@
 
 #include <zephyr/bluetooth/bluetooth.h>
 #include <zephyr/bluetooth/uuid.h>
+#include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/pm/device.h>
+#include <zephyr/sys/poweroff.h>
+#include <soc.h>
+#include "retained.h"
+#include <hal/nrf_gpio.h>
 
 #define SERVICE_UUID            0xfcd2      // BTHome service UUID
-#define IDX_TEMPL               4           // Index of lo byte of temp in service data
-#define IDX_TEMPH               5           // Index of hi byte of temp in service data
 
 
 
@@ -19,12 +25,12 @@
 
 static uint8_t service_data[] = { 
 	BT_UUID_16_ENCODE(SERVICE_UUID),
-	0x40,
-	0x02,	// Temperature
-	0x00,	// Low byte
-	0x00,   // High byte
-	0x03,	// Humidity
-	0xbf,	// 50.55%
+	0x44,	// bit 0 = 0 -> no encryption, bit 2 = 1 -> irregular data updates, bits 5-7 = 0b010 -> BTHome v2
+	0x3E,	// Count, 4B
+	0x00,	// Lowest byte
+	0x00,   // 
+	0x00,	// 
+	0x00,	// Highest byte
 	0x13,
 };
 
@@ -51,31 +57,64 @@ static void bt_ready(int err)
 	}
 }
 
-void main(void)
-{
-	int err;
-	int temp = 0;
 
-	printk("Starting BTHome test\n");
+
+#define SLEEP_S 1U
+
+int main(void)
+{
+	int rc;
+	const struct device *const cons = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
+
+	if (!device_is_ready(cons)) {
+		printk("%s: device not ready.\n", cons->name);
+		return 0;
+	}
+
+	bool retained_ok = retained_validate();
+
+	/* Increment for this boot attempt and update. */
+	retained.boots += 1;
+	retained_update();
+
+	printk("Retained data: %s\n", retained_ok ? "valid" : "INVALID");
+	printk("Boot count: %u\n", retained.boots);
+	printk("Off count: %u\n", retained.off_count);
+	printk("Active Ticks: %" PRIu64 "\n", retained.uptime_sum);
 
 	/* Initialize the Bluetooth Subsystem */
-	err = bt_enable(bt_ready);
-	if (err) {
-		printk("Bluetooth init failed (err %d)\n", err);
-		return;
+	rc = bt_enable(bt_ready);
+	if (rc) {
+		printk("Bluetooth init failed (err %d)\n", rc);
+		return -1;
 	}
 
-	for (;;) {
-		/* Simulate temperature from 0C to 25C */
-		service_data[IDX_TEMPH] = (temp * 100) >> 8;
-		service_data[IDX_TEMPL] = (temp * 100) & 0xff;
-		if (temp++ == 25) {
-			temp = 0;
-		}
-		err = bt_le_adv_update_data(ad, ARRAY_SIZE(ad), NULL, 0);
-		if (err) {
-			printk("Failed to update advertising data (err %d)\n", err);
-		}
-		k_sleep(K_MSEC(BT_GAP_ADV_SLOW_INT_MIN));
+	service_data[7] = (retained.boots  >> 24) & 0xff;
+	service_data[6] = (retained.boots  >> 16) & 0xff;
+	service_data[5] = (retained.boots  >> 8) & 0xff;
+	service_data[4] = (retained.boots  >> 0) & 0xff;
+
+	rc = bt_le_adv_update_data(ad, ARRAY_SIZE(ad), NULL, 0);
+	if (rc) {
+		printk("Failed to update advertising data (err %d)\n", rc);
 	}
+
+	/* Configure to generate PORT event (wakeup) on button 1 press. */
+	nrf_gpio_cfg_input(NRF_DT_GPIOS_TO_PSEL(DT_ALIAS(sw0), gpios),
+			   NRF_GPIO_PIN_PULLUP);
+	nrf_gpio_cfg_sense_set(NRF_DT_GPIOS_TO_PSEL(DT_ALIAS(sw0), gpios),
+			       NRF_GPIO_PIN_SENSE_LOW);
+
+	printk("Sleep %u s\n", SLEEP_S);
+	k_sleep(K_SECONDS(SLEEP_S));
+
+	printk("Entering system off; press BUTTON1 to restart\n");
+
+	/* Update the retained state */
+	retained.off_count += 1;
+	retained_update();
+
+	sys_poweroff();
+
+	return 0;
 }
